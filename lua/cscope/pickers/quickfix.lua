cfd1fb503036be0d24d0def573a06ce332b8447b
Arpith Alexander <Arpith.Alexander@mobileye.com>
2023-06-25 12:55:52 +0200

    quickfix: Use 'botright copen' instead of 'copen'

--- lua/cscope/pickers/quickfix.lua
+++ lua/cscope/pickers/quickfix.lua
@@ -3,7 +3,7 @@ local M = {}
 M.run = function(opts)
 	vim.fn.setqflist(opts.cscope.parsed_output, "r")
 	vim.fn.setqflist({}, "a", { title = opts.cscope.prompt_title })
-	vim.api.nvim_command("copen 5")
+	vim.api.nvim_command("botright copen")
 end
 
 return M
